c2927a35bd4b579b1ffe2d0d4b5b171edabc0e07
maxkazanc3 <maxkazanc3@gmail.com>
2025-07-25 04:43:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <div>
+      <Head><title>Slot Analiz</title></Head>
+      <main><h1>Slot Analiz Paneli</h1></main>
+    </div>
+  );
+}
